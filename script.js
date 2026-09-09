f1c6fd03ef268af3f00d669354f99dd88aafa8cc
ermeidis95-cmd <ermeidis95@gmail.com>
2026-09-09 19:43:46 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,31 +1,17 @@
-const darkModeToggle = document.getElementById("dark-mode-toggle");
+const button = document.getElementById("dark-mode-toggle");
 
-
-// Check whether the visitor previously selected dark mode
-
-if (localStorage.getItem("darkMode") === "enabled") {
-    document.body.classList.add("dark-mode");
-    darkModeToggle.textContent = "☀";
-}
-
-
-// Toggle dark mode
-
-darkModeToggle.addEventListener("click", function () {
+button.addEventListener("click", function () {
 
     document.body.classList.toggle("dark-mode");
 
     if (document.body.classList.contains("dark-mode")) {
 
-        localStorage.setItem("darkMode", "enabled");
-
-        darkModeToggle.textContent = "☀";
+        button.textContent = "☀";
 
     } else {
 
-        localStorage.setItem("darkMode", "disabled");
+        button.textContent = "☾";
 
-        darkModeToggle.textContent = "☾";
     }
 
 });
